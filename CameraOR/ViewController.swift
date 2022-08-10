770c344bd6d52d84a881d2bb8cc206b47d685b5a
andrew <andrew_mokryj@ukr.net>
2022-08-10 19:33:50 +0300

    feature: take photo for storyboard app

--- CameraOR/ViewController.swift
+++ CameraOR/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.backgroundColor = .systemBlue
+        button.setTitle("Open camera", for: .normal)
+        button.setTitleColor(.white, for: .normal)
     }
 
-
+    @IBAction func didTapButton(){
+        /// TODO: Change
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        imageView.image = image
+    }
+}
